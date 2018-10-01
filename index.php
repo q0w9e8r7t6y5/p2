bbdfe5e54aa292d9a65612624553bbec3ca5a358
Sishir Yeety <sishiryeety@MacBook-2.local>
2018-10-01 17:28:59 -0400

    new push

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <title>P2</title>
+    <meta charset="utf-8">
+    <link rel='stylesheet' href='https://bootswatch.com/4/lumen/bootstrap.min.css' type='text/css'>
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <h1>Sishir Yeety</h1>
+    <h2>Project 2</h2>
+
+</div>
+
+</body>
+
+</html>
