7600c0c4a9ab2f8f26329f6f6cd27388e1a6a5c5
q0w9e8r7t6y5 <syeety@me.com>
2018-10-03 17:15:16 -0400

    small fixes

--- order.php
+++ order.php
@@ -17,7 +17,9 @@ require 'logic.php';
 
 <style>
 
-    h5, h6 {font-weight: bold}
+    h5, h6 {
+        font-weight: bold
+    }
 
 </style>
 
@@ -47,7 +49,8 @@ require 'logic.php';
             <input type='radio'
                    name='type'
                    value='salad' <?php if (isset($type) and $type == 'salad') echo 'checked' ?>> Salad<br>
-        </fieldset><br>
+        </fieldset>
+        <br>
 
         <fieldset class='fill'>
             <h5>Meat & Tofu Selection</h5>
@@ -59,7 +62,8 @@ require 'logic.php';
                 <option value='barbacoa' <?php if ($fill == 'barbacoa') echo 'selected' ?>>Barbacoa</option>
                 <option value='sofritas' <?php if ($fill == 'sofritas') echo 'selected' ?>>Sofritas(Tofu)</option>
             </select>
-        </fieldset><br>
+        </fieldset>
+        <br>
 
         <fieldset class='rice'>
             <h5>Fillings</h5>
@@ -74,7 +78,8 @@ require 'logic.php';
             <input type='radio'
                    name='rice'
                    value='None' <?php if (isset($rice) and $rice == 'None') echo 'checked' ?>> None<br>
-        </fieldset><br>
+        </fieldset>
+        <br>
 
         <fieldset class='beans'>
             <h6>Bean</h6>
@@ -89,7 +94,8 @@ require 'logic.php';
             <input type='radio'
                    name='beans'
                    value='None' <?php if (isset($beans) and $beans == 'None') echo 'checked' ?>> None<br>
-        </fieldset><br>
+        </fieldset>
+        <br>
 
         <fieldset class='veggies'>
             <h6>Fajita Veggies</h6>
@@ -100,7 +106,8 @@ require 'logic.php';
             <input type='radio'
                    name='veggies'
                    value='No' <?php if (isset($veggies) and $veggies == 'No') echo 'checked' ?>> No<br>
-        </fieldset><br>
+        </fieldset>
+        <br>
 
         <fieldset id="toppings">
             <h6>Toppings</h6>
@@ -128,7 +135,8 @@ require 'logic.php';
             <input type='checkbox'
                    name='toppings[]'
                    value='cheese' <?php if (strstr($results, 'cheese')) echo 'checked' ?>> Shredded Cheese<br>
-        </fieldset><br>
+        </fieldset>
+        <br>
 
         <fieldset id="comment">
             <h5>Special Instructions</h5>
@@ -183,7 +191,7 @@ require 'logic.php';
                 </tr>
                 <tr>
                     <td>Total (with tax):</td>
-<!--                    <td>--><?//= ucfirst($total) ?>
+                    <!--                    <td>--><? //= ucfirst($total) ?>
                     <td>
                         <?php
                         if ($fill != "sofritas")
